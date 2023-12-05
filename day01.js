7edf5437d2483f2937280960bcc25515115ab0fb
vanesavillarreal <vanessa_diesver@hotmail.com>
2023-12-05 13:55:53 -0700

    Adds convertion to number of 2 digits

--- day01.js
+++ day01.js
@@ -1,21 +1,27 @@
 import { array } from "./input.js";
 
-// const words = ["1abc2", "pqr3stu8vwx"]; //12 38
+const words = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]; //12 38 15 77
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let numbersIntoWords = []
+let firstNumber = '';
+let secondNumber = '';
 
-
+//recorrer el la palabra hasta encontrar el primer numero
 const getNumberFromWords = (word) => {
     let numberInWord = '';
     numbers.forEach( number => {
-        // console.log(word.includes(number))
         if (word.includes(number)){
-            let x = word.indexOf(number)
-            // console.log(x)
-            numberInWord +=  word.slice(x, x + 1)
+            let indexChar = word.indexOf(number)
+            numberInWord +=  word.slice(indexChar, indexChar + 1)
         }
     })
+    if(numberInWord.length > 2){
+        numberInWord = numberInWord[0] + numberInWord[numberInWord.length - 1];
+    }
+    if(numberInWord.length === 1){
+        numberInWord += numberInWord[0];
 
+    }
     numbersIntoWords.push(Number(numberInWord))
 }
 
@@ -24,7 +30,7 @@ array.forEach(string => {
     getNumberFromWords(string);
 });
 
-console.log("number:", numbersIntoWords)
+// console.log("number:", numbersIntoWords)
 const totalSum = numbersIntoWords.reduce(
     (accumulator, currentValue) => accumulator + currentValue
 );
