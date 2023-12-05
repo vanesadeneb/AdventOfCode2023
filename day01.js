4cde6801062a7fef458f0aa4c5d254e2f7ada474
vanesavillarreal <vanessa_diesver@hotmail.com>
2023-12-05 14:01:28 -0700

    Removes unused variables

--- day01.js
+++ day01.js
@@ -3,8 +3,6 @@ import { array } from "./input.js";
 const words = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]; //12 38 15 77
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let numbersIntoWords = []
-let firstNumber = '';
-let secondNumber = '';
 
 //recorrer el la palabra hasta encontrar el primer numero
 const getNumberFromWords = (word) => {
